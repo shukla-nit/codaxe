55ebb56e1dba6f23200e09d1fde92dbfaeb97d78
shukla-nit <shukla-nit@users.noreply.github.com>
2017-08-22 11:28:49 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hackerrank
+//
+//  Created by Rakesh Shukla on 22/08/17.
+//  Copyright © 2017 Rakesh Shukla. All rights reserved.
+//
+
+#include <iostream>
+#include "organizing-containers-of-balls.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+  
+    organizing_containers_of_balls obj;
+    obj.solve();
+    return 0;
+}
